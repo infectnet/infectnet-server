e038028bf1afd880488950589e16c6a652ff1423
István Lakatos <atyep95@msn.com>
2016-09-29 12:44:58 +0200

    Added forgotten final modifier

--- server-core/src/main/java/io/infectnet/server/core/ApplicationStarter.java
+++ server-core/src/main/java/io/infectnet/server/core/ApplicationStarter.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 public class ApplicationStarter {
 
-    private List<RestController> restControllers;
+    private final List<RestController> restControllers;
 
     @Inject
     public ApplicationStarter() {
