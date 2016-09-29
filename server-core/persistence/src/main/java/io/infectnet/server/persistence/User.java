dd383cdcc275c2b8b2b01f707e71c0f8985fb2fd
marianna-szabo-deik <szabo.marica@gmail.com>
2016-09-29 18:12:06 +0200

    Implemented UserStorage

--- server-core/persistence/src/main/java/io/infectnet/server/persistence/User.java
+++ server-core/persistence/src/main/java/io/infectnet/server/persistence/User.java
@@ -1,4 +1,44 @@
 package io.infectnet.server.persistence;
 
 public class User {
+
+    private String userName;
+
+    private String email;
+
+    private String password;
+
+    private LocalDateTime registrationDate;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(LocalDateTime registrationDate) {
+        this.registrationDate = registrationDate;
+    }
 }
--- server-core/persistence/src/main/java/io/infectnet/server/persistence/UserStorage.java
+++ server-core/persistence/src/main/java/io/infectnet/server/persistence/UserStorage.java
@@ -1,12 +1,15 @@
 package io.infectnet.server.persistence;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserStorage {
 
     List<User> getAllUsers();
 
-    User getUserById(Integer id);
+    Optional<User> getUserByUserName(String userName);
+
+    Optional<User> getUserByEmail(String email);
 
     void saveUser(User user);
 
