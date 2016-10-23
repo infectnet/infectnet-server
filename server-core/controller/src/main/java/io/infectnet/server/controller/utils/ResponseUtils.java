93539316efbe170e092a43bd94767cc19e68946d
Attila Bagossy <bagossyattila@outlook.com>
2016-10-23 17:50:46 +0200

    Documented ResponseUtils and added a new Error class

--- server-core/controller/src/main/java/io/infectnet/server/controller/utils/ResponseUtils.java
+++ server-core/controller/src/main/java/io/infectnet/server/controller/utils/ResponseUtils.java
@@ -4,6 +4,10 @@ import org.apache.commons.lang3.StringUtils;
 
 import spark.Response;
 
+/**
+ * Utility class exposing various response-related helper methods that contain frequently used
+ * shared functionality among controllers.
+ */
 public final class ResponseUtils {
   private ResponseUtils() {
     /**
@@ -11,10 +15,21 @@ public final class ResponseUtils {
      */
   }
 
+  /**
+   * Returns an empty {@code String} and sets the response status code to {@code 200 OK}.
+   * @param resp the response object
+   * @return an empty String
+   */
   public static String sendEmptyOk(Response resp) {
     return sendEmptyWithStatusCode(resp, 200);
   }
 
+  /**
+   * Returns an empty {@code String} and sets the response status code to the specified value.
+   * @param resp the response object
+   * @param statusCode the status code to be set on the response
+   * @return an empty String
+   */
   public static String sendEmptyWithStatusCode(Response resp, int statusCode) {
    resp.status(statusCode);
 
