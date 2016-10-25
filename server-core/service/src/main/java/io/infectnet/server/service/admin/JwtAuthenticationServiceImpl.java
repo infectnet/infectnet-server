d5a0d8c500e481110293f9f04ecf2c3b9d11dd89
Attila Bagossy <bagossyattila@outlook.com>
2016-10-25 20:29:10 +0200

    Finished implementation of JwtAuthenticationService

--- server-core/service/src/main/java/io/infectnet/server/service/admin/JwtAuthenticationServiceImpl.java
+++ server-core/service/src/main/java/io/infectnet/server/service/admin/JwtAuthenticationServiceImpl.java
@@ -5,34 +5,45 @@ import io.infectnet.server.common.configuration.ConfigurationHolder;
 import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 
-import java.util.Base64;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.util.Collections;
+import java.util.Date;
 import java.util.Objects;
 import java.util.Optional;
 
 public class JwtAuthenticationServiceImpl implements AuthenticationService {
   private static final String JWT_SECRET_KEY = "admin_jwt_secret";
 
-  private final Base64.Encoder base64Encoder;
+  private static final String JWT_USERNAME_FIELD = "username";
 
-  public JwtAuthenticationServiceImpl() {
-    this.base64Encoder = Base64.getEncoder();
-  }
+  private static final String JWT_ISSUER = "infectnet";
 
   @Override
   public Optional<String> login(String username, String password) {
+    if (credentialsMatch(username, password)) {
+      AdminCredentials credentials = AdminCredentials.fetchFromConfiguration();
 
+      String token = produceToken(credentials);
 
-    return null;
+      return Optional.of(token);
+    } else {
+      return Optional.empty();
+    }
   }
 
   @Override
   public boolean isAuthenticated(String token) {
     final String nonNullToken = Objects.requireNonNull(token);
 
+    AdminCredentials credentials = AdminCredentials.fetchFromConfiguration();
+
     try {
       Jwts.parser()
-          .setSigningKey(base64Encoder.encode(fetchJwtSecret().getBytes()))
-          .parse(nonNullToken);
+          .requireIssuer(JWT_ISSUER)
+          .require(JWT_USERNAME_FIELD, credentials.getUsername())
+          .setSigningKey(fetchJwtSecret().getBytes())
+          .parseClaimsJws(nonNullToken);
 
       return true;
     } catch (JwtException | IllegalArgumentException e) {
@@ -42,12 +53,69 @@ public class JwtAuthenticationServiceImpl implements AuthenticationService {
 
   @Override
   public Optional<String> renewToken(String token) {
-    return null;
+    if (!isAuthenticated(Objects.requireNonNull(token))) {
+      return Optional.empty();
+    } else {
+      String newToken = produceToken(AdminCredentials.fetchFromConfiguration());
+
+      return Optional.of(newToken);
+    }
+  }
+
+  private String produceToken(AdminCredentials credentials) {
+    return Jwts.builder()
+        .setIssuer(JWT_ISSUER)
+        .setClaims(Collections.singletonMap(JWT_USERNAME_FIELD, credentials.getUsername()))
+        .setExpiration(calculateExpirationDate())
+        .compact();
+  }
+
+  private Date calculateExpirationDate() {
+    Instant expirationInstant = Instant.now().plus(1, ChronoUnit.HOURS);
+
+    return Date.from(expirationInstant);
+  }
+
+  private boolean credentialsMatch(String username, String password) {
+    AdminCredentials credentials = AdminCredentials.fetchFromConfiguration();
+
+    return username.equals(credentials.getUsername())
+        && password.equals(credentials.getPassword());
   }
 
   private String fetchJwtSecret() {
     Configuration config = ConfigurationHolder.INSTANCE.getActiveConfiguration();
 
     return config.get(JWT_SECRET_KEY);
+  }
+
+  private static class AdminCredentials {
+    private static final String USERNAME_KEY = "admin_username";
+
+    private static final String PASSWORD_KEY = "admin_password";
+
+    private final String username;
+
+    private final String password;
+
+    static AdminCredentials fetchFromConfiguration() {
+      Configuration config = ConfigurationHolder.INSTANCE.getActiveConfiguration();
+
+      return new AdminCredentials(config.get(USERNAME_KEY), config.get(PASSWORD_KEY));
+    }
+
+    private AdminCredentials(String username, String password) {
+      this.username = username;
+
+      this.password = password;
+    }
+
+    String getUsername() {
+      return username;
+    }
+
+    String getPassword() {
+      return password;
+    }
   }
 }
