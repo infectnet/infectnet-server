4ae2e64382be7907e3eee806fa152c5ca04372c7
Attila Bagossy <bagossyattila@outlook.com>
2016-11-07 23:13:43 +0100

    Script generation related classes added

---
+++ server-core/engine/src/main/java/io/infectnet/server/engine/dsl/script/ScriptGenerationFailedException.java
@@ -0,0 +1,7 @@
+package io.infectnet.server.engine.dsl.script;
+
+public class ScriptGenerationFailedException extends Exception {
+  public ScriptGenerationFailedException(Throwable cause) {
+    super(cause);
+  }
+}
