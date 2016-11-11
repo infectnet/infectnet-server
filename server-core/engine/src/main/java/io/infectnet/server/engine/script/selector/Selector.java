9fc8332f422264f016789ac70e33763d21d88abe
Attila Bagossy <bagossyattila@outlook.com>
2016-11-12 00:11:31 +0100

    Base classes added to the  engine.script.selector package

---
+++ server-core/engine/src/main/java/io/infectnet/server/engine/script/selector/Selector.java
@@ -0,0 +1,41 @@
+package io.infectnet.server.engine.script.selector;
+
+import io.infectnet.server.engine.player.Player;
+
+/**
+ * Base class for expressions that can be used in SFA blocks
+ * (see {@link io.infectnet.server.engine.script.dsl.SelectFilterActionBlock}). Please note that all
+ * public methods will be available in the Groovy DSL. The {@link Player} passed to an instance is
+ * the player whose code uses the instance.
+ * <p>
+ * Descendants are free to provide any number of methods that return a {@link java.util.Collection}.
+ * These methods will be the actual selectors.
+ * </p>
+ * <p>
+ * If a selector method does not take arguments then it must be implemented as a getter, so instead
+ * of a method named {@code workers()}, the method {@code getWorkers()} should be declared. However
+ * when a selector method does take arguments this restriction does not apply.
+ * </p>
+ * <p>
+ * Please provide both singular and plural named versions of the same selector. This means that both
+ * {@code getWorker()} and {@code getWorkers()} should be declred with the same functionality.
+ * </p>
+ * <p>
+ * Groovy enables users to use named parameters, therefore please expose descriptive parameter
+ * names.
+ * </p>
+ */
+public abstract class Selector {
+  protected final Player player;
+
+  /**
+   * Constructs a new instance with the specified {@code Player}.
+   * @param player the {@code Player} whose code uses the instance
+   */
+  Selector(Player player) {
+    this.player = player;
+  }
+  public Player getPlayer() {
+    return player;
+  }
+}
