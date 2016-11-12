dc274e1631d224c6222effa7eeb9a51f4327c825
Attila Bagossy <bagossyattila@outlook.com>
2016-11-12 21:03:49 +0100

    :tada: The Game Loop is added :tada:

---
+++ server-core/engine/src/main/groovy/io/infectnet/server/engine/Request.java
@@ -0,0 +1,7 @@
+package io.infectnet.server.engine;
+
+/**
+ * Created by Attila on 2016. 11. 12..
+ */
+public class Request {
+}
---
+++ server-core/engine/src/main/java/io/infectnet/server/engine/GameLoop.java
@@ -0,0 +1,64 @@
+package io.infectnet.server.engine;
+
+import io.infectnet.server.engine.entity.wrapper.Action;
+import io.infectnet.server.engine.script.code.Code;
+import io.infectnet.server.engine.script.code.CodeRepository;
+import io.infectnet.server.engine.script.execution.ScriptExecutor;
+import io.infectnet.server.engine.util.ListenableQueue;
+
+class GameLoop {
+  private final ListenableQueue<Action> actionQueue;
+
+  private final ListenableQueue<Request> requestQueue;
+
+  private final CodeRepository codeRepository;
+
+  private final ScriptExecutor scriptExecutor;
+
+  public GameLoop(ListenableQueue<Action> actionQueue, ListenableQueue<Request> requestQueue,
+                  CodeRepository codeRepository, ScriptExecutor scriptExecutor) {
+    this.actionQueue = actionQueue;
+
+    this.requestQueue = requestQueue;
+
+    this.codeRepository = codeRepository;
+
+    this.scriptExecutor = scriptExecutor;
+  }
+
+  public void loop() {
+    /*
+     * #1 Run Scripts
+     *
+     * The action queue will be filled with Action instances made by the Scripts.
+     */
+    for (Code code : codeRepository.getAllCodes()) {
+      if (code.isRunnable()) {
+        scriptExecutor.execute(code.getScript().get(), code.getOwner());
+      }
+    }
+
+    /*
+     * #2 Process Actions
+     *
+     * The Actions will be dispatched towards the various systems via the registered listeners. The
+     * systems will create Requests and place them in the request queue.
+     */
+    actionQueue.processAll();
+
+    /*
+     * #3 Process Requests
+     *
+     * Requests crafted from Actions have no dependencies on the previous state of the World or the
+     * Entity System. The request queue dispatches the requests to the various systems which will
+     * modify the World and the Entities.
+     */
+    requestQueue.processAll();
+
+    /*
+     * #4 Send results
+     *
+     * TODO
+     */
+  }
+}
