68662a3b8ccff6e0e970f30152835a559a5f8dfe
Attila Bagossy <bagossyattila@outlook.com>
2016-11-12 22:33:26 +0100

    Added functionality to the ScriptExecutorImpl

--- server-core/engine/src/main/java/io/infectnet/server/engine/script/execution/ScriptExecutorImpl.java
+++ server-core/engine/src/main/java/io/infectnet/server/engine/script/execution/ScriptExecutorImpl.java
@@ -1,11 +1,58 @@
 package io.infectnet.server.engine.script.execution;
 
+import groovy.lang.Binding;
 import groovy.lang.Script;
 import io.infectnet.server.engine.player.Player;
+import io.infectnet.server.engine.script.selector.Selector;
+import io.infectnet.server.engine.script.selector.SelectorFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.function.Supplier;
 
 public class ScriptExecutorImpl implements ScriptExecutor {
+  private final Set<SelectorFactory<? extends Selector>> selectorFactories;
+
+  private final Map<String, Binding> bindingMap;
+
+  private final Supplier<Binding> bindingSupplier;
+
+  public ScriptExecutorImpl(Set<SelectorFactory<? extends Selector>> selectorFactories,
+                            Supplier<Binding> bindingSupplier) {
+    this.selectorFactories = selectorFactories;
+
+    this.bindingSupplier = bindingSupplier;
+
+    this.bindingMap = new HashMap<>();
+  }
+
   @Override
   public void execute(Script script, Player owner) {
+    Binding binding = createOrGetBinding(owner);
 
+    script.setBinding(binding);
+
+    script.run();
+
+    script.setBinding(null);
+  }
+
+  private Binding createOrGetBinding(Player player) {
+    Binding binding = bindingMap.get(player.getUsername());
+
+    return binding != null ? binding : createBinding(player);
+  }
+
+  private Binding createBinding(Player player) {
+    Binding binding = bindingSupplier.get();
+
+    for (SelectorFactory<? extends Selector> factory : selectorFactories) {
+      binding.setVariable(factory.getName(), factory.forPlayer(player));
+    }
+
+    bindingMap.put(player.getUsername(), binding);
+
+    return binding;
   }
 }
