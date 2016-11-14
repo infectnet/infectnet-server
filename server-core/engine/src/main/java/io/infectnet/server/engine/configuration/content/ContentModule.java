8f6edeb736adbcbf7d36c91b10fc023f33558e45
Attila Bagossy <bagossyattila@outlook.com>
2016-11-14 19:34:38 +0100

    Added the Function.identity() as player initializer fn

--- server-core/engine/src/main/java/io/infectnet/server/engine/configuration/content/ContentModule.java
+++ server-core/engine/src/main/java/io/infectnet/server/engine/configuration/content/ContentModule.java
@@ -1,7 +1,17 @@
 package io.infectnet.server.engine.configuration.content;
 
+import io.infectnet.server.engine.player.Player;
+
+import java.util.function.Function;
+import javax.inject.Singleton;
 import dagger.Module;
+import dagger.Provides;
 
 @Module(includes = {SelectorModule.class, DslModule.class, SystemModule.class})
 public class ContentModule {
+  @Provides
+  @Singleton
+  public static Function<Player, Player> providesIdentityPlayerInitializer() {
+    return Function.identity();
+  }
 }
