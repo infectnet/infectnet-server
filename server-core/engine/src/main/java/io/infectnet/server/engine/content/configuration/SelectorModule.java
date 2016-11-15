d941bc58329cad8a3098e09ba75e8bf70f553f03
Attila Bagossy <bagossyattila@outlook.com>
2016-11-15 22:20:40 +0100

    Small fixes, functionality left untouched

--- server-core/engine/src/main/java/io/infectnet/server/engine/content/configuration/SelectorModule.java
+++ server-core/engine/src/main/java/io/infectnet/server/engine/content/configuration/SelectorModule.java
@@ -1,13 +1,6 @@
 package io.infectnet.server.engine.content.configuration;
 
-import io.infectnet.server.engine.core.script.selector.Selector;
-import io.infectnet.server.engine.core.script.selector.SelectorFactory;
-
-import java.util.Collections;
-import java.util.Set;
 import dagger.Module;
-import dagger.Provides;
-import dagger.multibindings.ElementsIntoSet;
 
 @Module
 public class SelectorModule {
