5e504a07bbf7bcac111f072978ca1fecd507a4c3
Attila Bagossy <bagossyattila@outlook.com>
2016-11-15 21:51:53 +0100

    Owner can now be set on an Entity

--- server-core/engine/src/main/java/io/infectnet/server/engine/core/entity/component/OwnerComponent.java
+++ server-core/engine/src/main/java/io/infectnet/server/engine/core/entity/component/OwnerComponent.java
@@ -6,7 +6,11 @@ import io.infectnet.server.engine.core.player.Player;
  * Component holding the owner of an {@link io.infectnet.server.engine.core.entity.Entity}.
  */
 public class OwnerComponent {
-  private final Player owner;
+  private Player owner;
+
+  public OwnerComponent() {
+    this.owner = null;
+  }
 
   public OwnerComponent(Player owner) {
     this.owner = owner;
@@ -14,5 +18,9 @@ public class OwnerComponent {
 
   public Player getOwner() {
     return owner;
+  }
+
+  public void setOwner(Player owner) {
+    this.owner = owner;
   }
 }
