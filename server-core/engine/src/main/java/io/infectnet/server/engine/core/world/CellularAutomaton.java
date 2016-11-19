007f3d5d2d7f674a3cc6d4fc7a4a783d08ee1607
marianna-szabo-deik <szabo.marica@gmail.com>
2016-11-19 17:37:37 +0100

    Follow-up commit

--- server-core/engine/src/main/java/io/infectnet/server/engine/core/world/CellularAutomaton.java
+++ server-core/engine/src/main/java/io/infectnet/server/engine/core/world/CellularAutomaton.java
@@ -2,6 +2,9 @@ package io.infectnet.server.engine.core.world;
 
 import static java.lang.Math.random;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * This class represents a Cellular Automaton, which is used in the generation of the Map tiles.
  */
@@ -62,26 +65,23 @@ class CellularAutomaton {
    * @return a boolean array containing the data about all tiles that were generated.
    */
   boolean[][] generateMap(){
-    boolean[][] map = new boolean[height][width];
+    map = new boolean[height][width];
 
-    map = initialiseMap(map);
+    initializeMap();
 
     for(int i = 0; i < numberOfSteps; ++i){
-      map = doSimulationStep(map);
+      doSimulationStep();
     }
 
-    map = eliminateIslands(map);
+    map = eliminateIslands();
 
-    //TODO check if the cave system is big enough
     return map;
   }
 
   /**
    * Initializes the array with true and false values placed at random.
-   * @param map the array to initialize
-   * @return the initialized array, with true and false values
    */
-  private boolean[][] initialiseMap(boolean[][] map){
+  private void initializeMap(){
     for(int x = 0; x < width; ++x){
       for(int y = 0; y < height; ++y){
         if(random() < chanceToStartAlive){
@@ -89,23 +89,20 @@ class CellularAutomaton {
         }
       }
     }
-    return map;
   }
 
   /**
    * Performs a simulation step, in which it creates a new map using the values of the original,
    * now old map and the pre-set values of deathLimit and birthLimit.
-   * @param oldMap the array containing the value of the map before the step
-   * @return a new array containing the values calculated by the values of the old map
    */
-  private boolean[][] doSimulationStep(boolean[][] oldMap){
+  private void doSimulationStep(){
     boolean[][] newMap = new boolean[height][width];
 
-    for(int y = 0; y < oldMap.length; ++y){
-      for(int x = 0; x < oldMap[0].length; ++x){
-        int aliveNeighbours = countAliveNeighbours(oldMap, x, y);
+    for(int y = 0; y < map.length; ++y){
+      for(int x = 0; x < map[0].length; ++x){
+        int aliveNeighbours = countAliveNeighbours(x, y);
 
-        if(oldMap[y][x]){
+        if(map[y][x]){
           newMap[y][x] = aliveNeighbours >= deathLimit;
         }
         else{
@@ -113,17 +110,16 @@ class CellularAutomaton {
         }
       }
     }
-    return newMap;
+    map = newMap;
   }
 
   /**
    * Counts the number of cells in a ring around (x,y) that are alive.
-   * @param map the array of the map
    * @param x the first coordinate
    * @param y the second coordinate
    * @return the number of the alive cells found surrounding the (x,y) cell
    */
-  private int countAliveNeighbours(boolean[][] map, int x, int y){
+  private int countAliveNeighbours(int x, int y){
     int count = 0;
 
     for(int i = -1; i < 2; ++i){
@@ -134,7 +130,7 @@ class CellularAutomaton {
         if(!isMiddleCell(i, j)){
           if(isInvalidCoordinate(neighbourX, neighbourY)){
             count = count + 1;
-          } else if(map[neighbourX][neighbourY]){
+          } else if(!map[neighbourX][neighbourY]){
             count = count + 1;
           }
         }
@@ -167,19 +163,172 @@ class CellularAutomaton {
 
   /**
    * Creates a new map without the isolated caves, which were on the previous map.
-   * @param oldMap the array with the generated values
-   * @return a new array containing only one cave system
    */
-  private boolean[][] eliminateIslands(boolean[][] oldMap) {
-    boolean[][] newMap = new boolean[height][width];
-    //TODO define  a starting point!!!
-    newMap = floodFillWholeCave(oldMap, newMap);
+  private boolean[][] eliminateIslands() {
+    float ratio = 0f;
+    Position startingPosition = new Position(0,0);
 
-    return newMap;
+    while(ratio < 0.6f){
+      finalMap = new boolean[height][width];
+
+      startingPosition = findNextCavePosition(startingPosition);
+      if(startingPosition == null){
+        break;
+      }
+
+      floodFillWholeCave(startingPosition);
+      ratio = countCaveCellRatio();
+    }
+
+    return finalMap;
   }
 
-  private boolean[][] floodFillWholeCave(boolean[][] oldMap, boolean[][] newMap){
-    //TODO
-    return newMap;
+  /**
+   * Implementation of the classic flood-fill algorithm. It explores the cave system
+   * from the starting position, storing the found positions in a new array called finalMap.
+   * @param position the starting position
+   */
+  private void floodFillWholeCave(Position position){
+    if(!isValidPosition(position)){
+      return;
+    }
+
+    Queue<Position> queue = new LinkedList<>();
+    queue.add(position);
+
+    while(!queue.isEmpty()){
+      System.out.print(queue.size()+" ");
+      Position pos = queue.remove();
+      int h = pos.getH();
+      int w = pos.getW();
+
+      if(isNotVisitedCave(pos)){
+        finalMap[h][w] = true;
+      }
+
+      Position southPos = pos.stepSouth();
+      Position westPos = pos.stepWest();
+      Position northPos = pos.stepNorth();
+      Position eastPos = pos.stepEast();
+      if(isValidPosition(southPos) && isNotVisitedCave(southPos)){
+        queue.add(southPos);
+      }
+      if(isValidPosition(westPos) && isNotVisitedCave(westPos)){
+        queue.add(westPos);
+      }
+      if(isValidPosition(northPos) && isNotVisitedCave(northPos)){
+        queue.add(northPos);
+      }
+      if(isValidPosition(eastPos) && isNotVisitedCave(eastPos)){
+        queue.add(eastPos);
+      }
+    }
+  }
+
+  /**
+   * Checks if the Position given points to a cave in the map but to a rock on the new map.
+   * @param pos the position to check
+   * @return true if the value of the Position was not yet evaluated with the flood-fill algorithm, false otherwise.
+   */
+  private boolean isNotVisitedCave(Position pos) {
+    return map[pos.getH()][pos.getW()] && !finalMap[pos.getH()][pos.getW()];
+  }
+
+  /**
+   * Checks if the Position is on the map, so not beyond its borders
+   * @param position the position to check
+   * @return true if it is valid, false otherwise
+   */
+  private boolean isValidPosition(Position position) {
+    return position.getH() < height && position.getW() < width && position.getH() >= 0 && position.getW() >= 0;
+  }
+
+  /**
+   * Searches through the map for a position representing a cave, that have not been visited
+   * by the algorithm.
+   * @param oldPosition the previously found position
+   * @return the new Position, which is a not yet visited cave
+   */
+  private Position findNextCavePosition(Position oldPosition){
+    for(int i = oldPosition.getH(); i < height; ++i){
+      for(int j = 0; j < width; ++j){
+        if(map[i][j] && !finalMap[i][j]){
+          return new Position(i,j);
+        }
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Counts the visited cave cells, stored in the finalMap array and calculates its ratio, compared to the whole array.
+   * @return float the ratio of the cave system to all cells of the map.
+   */
+  private float countCaveCellRatio() {
+    float counter = 0;
+    float numberOfAllCells = height*width;
+
+    for(int i = 0; i < height; ++i) {
+      for (int j = 0; j < width; ++j) {
+        if(finalMap[i][j]){
+          counter++;
+        }
+      }
+    }
+
+    return counter / numberOfAllCells;
+  }
+
+  /**
+   * An inner class to hold two coordinates together.
+   */
+  private class Position{
+    private int h;
+    private int w;
+
+    Position(int h, int w){
+      this.h = h;
+      this.w = w;
+    }
+
+    /**
+     * Creates this Position's neighbour, stepping one step south.
+     * @return the southern neighbour
+     */
+    Position stepSouth(){
+      return new Position(h+1, w);
+    }
+
+    /**
+     * Creates this Position's neighbour, stepping one step west.
+     * @return the western neighbour
+     */
+    Position stepWest(){
+      return new Position(h, w-1);
+    }
+
+    /**
+     * Creates this Position's neighbour, stepping one step north.
+     * @return the northern neighbour
+     */
+    Position stepNorth(){
+      return new Position(h-1, w);
+    }
+
+    /**
+     * Creates this Position's neighbour, stepping one step east.
+     * @return the eastern neighbour
+     */
+    Position stepEast(){
+      return new Position(h, w+1);
+    }
+
+    int getH() {
+      return h;
+    }
+
+    int getW() {
+      return w;
+    }
   }
 }
