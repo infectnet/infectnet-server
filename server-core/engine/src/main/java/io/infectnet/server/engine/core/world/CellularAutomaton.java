9b5a2a706b067687aa72c502e3df80d26d7f03a9
szabom5 <szabo.marica@gmail.com>
2016-11-21 21:23:11 +0100

    Position class and PositionComponent created

--- server-core/engine/src/main/java/io/infectnet/server/engine/core/world/CellularAutomaton.java
+++ server-core/engine/src/main/java/io/infectnet/server/engine/core/world/CellularAutomaton.java
@@ -34,7 +34,7 @@ public class CellularAutomaton implements WorldGeneratorStrategy {
   /**
    * The minimum limit of the coherent cave system
    */
-  public static final float RATIO_LIMIT = 0.6f;
+  private static final float RATIO_LIMIT = 0.6f;
 
   /**
    * The height of the world.
@@ -273,77 +273,5 @@ public class CellularAutomaton implements WorldGeneratorStrategy {
     }
 
     return counter / numberOfAllCells;
-  }
-
-  /**
-   * An inner class to hold two coordinates together.
-   */
-  private class Position{
-    private final int h;
-    private final int w;
-
-    Position(int h, int w){
-      this.h = h;
-      this.w = w;
-    }
-
-    /**
-     * Creates this Position's neighbour, stepping one step south.
-     * @return the southern neighbour
-     */
-    Position stepSouth(){
-      return new Position(h+1, w);
-    }
-
-    /**
-     * Creates this Position's neighbour, stepping one step west.
-     * @return the western neighbour
-     */
-    Position stepWest(){
-      return new Position(h, w-1);
-    }
-
-    /**
-     * Creates this Position's neighbour, stepping one step north.
-     * @return the northern neighbour
-     */
-    Position stepNorth(){
-      return new Position(h-1, w);
-    }
-
-    /**
-     * Creates this Position's neighbour, stepping one step east.
-     * @return the eastern neighbour
-     */
-    Position stepEast(){
-      return new Position(h, w+1);
-    }
-
-    int getH() {
-      return h;
-    }
-
-    int getW() {
-      return w;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) return true;
-      if (o == null || getClass() != o.getClass()) return false;
-
-      Position position = (Position) o;
-
-      if (h != position.h) return false;
-      return w == position.w;
-
-    }
-
-    @Override
-    public int hashCode() {
-      int result = h;
-      result = 31 * result + w;
-      return result;
-    }
   }
 }
