3991801da300b4bfbea0951f0085276823766ee4
István Lakatos <atyep95@msn.com>
2016-12-04 17:18:58 +0100

    Added entity attribute querying methods to EntityWrapper

--- server-core/engine/src/main/java/io/infectnet/server/engine/core/entity/wrapper/EntityWrapper.java
+++ server-core/engine/src/main/java/io/infectnet/server/engine/core/entity/wrapper/EntityWrapper.java
@@ -1,6 +1,8 @@
 package io.infectnet.server.engine.core.entity.wrapper;
 
+import io.infectnet.server.engine.core.entity.Category;
 import io.infectnet.server.engine.core.entity.Entity;
+import io.infectnet.server.engine.core.world.Position;
 
 import java.util.function.BiConsumer;
 
@@ -8,8 +10,8 @@ import java.util.function.BiConsumer;
  * Wrapper (or proxy class) for {@link Entity} in order to make it safe to expose them to
  * Groovy DSL code.
  * <p>
- *   Descendants of this class <b>must</b> be written in Groovy so they can implement traits and
- *   have meta class.
+ * Descendants of this class <b>must</b> be written in Groovy so they can implement traits and
+ * have meta class.
  * </p>
  */
 public abstract class EntityWrapper {
@@ -47,6 +49,54 @@ public abstract class EntityWrapper {
    */
   public String getType() {
     return wrappedEntity.getTypeComponent().getName();
+  }
+
+  /**
+   * Returns the wrapped {@link Entity}'s category.
+   * @return the category
+   */
+  public Category getCategory() {
+    return wrappedEntity.getTypeComponent().getCategory();
+  }
+
+  /**
+   * Returns the wrapped {@link Entity}'s current position.
+   * @return the current position
+   */
+  public Position getPosition() {
+    return wrappedEntity.getPositionComponent().getPosition();
+  }
+
+  /**
+   * Returns the wrapped {@link Entity}'s current health points.
+   * @return the current health
+   */
+  public int getHealth() {
+    return wrappedEntity.getHealthComponent().getHealth();
+  }
+
+  /**
+   * Returns the wrapped {@link Entity}'s maximum inventory capacity.
+   * @return the max inventory capacity
+   */
+  public int getMaxInvetoryCapacity() {
+    return wrappedEntity.getInventoryComponent().getCapacity();
+  }
+
+  /**
+   * Returns the wrapped {@link Entity}'s free inventory capacity.
+   * @return the free inventory capacity
+   */
+  public int getFreeInventoryCapacity() {
+    return wrappedEntity.getInventoryComponent().getFreeCapacity();
+  }
+
+  /**
+   * Returns the wrapped {@link Entity}'s view radius.
+   * @return the view radius
+   */
+  public int getViewRadius() {
+    return wrappedEntity.getViewComponent().getViewRadius();
   }
 
   public class WrapperState {
