c259d02d9d18926fb84987a15af790b5fa4b4f0e
Marianna Szabó <szabo.marica@gmail.com>
2016-12-05 18:57:49 +0100

    Method Complexity decreased

--- server-core/engine/src/main/java/io/infectnet/server/engine/core/world/customizer/NestCustomizer.java
+++ server-core/engine/src/main/java/io/infectnet/server/engine/core/world/customizer/NestCustomizer.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
 import java.util.Set;
 
@@ -44,6 +45,28 @@ public class NestCustomizer implements WorldCustomizer {
   }
 
   /**
+   * Returns a Position to place a Nest, chosen at random.
+   * @return the chosen Position in an Optional
+   */
+  public Optional<Position> getRandomNestPosition() {
+    Position base = basePositions.get(new Random().nextInt(basePositions.size() - 1));
+    basePositions.remove(base);
+
+    Iterator<Position> it = basePositions.iterator();
+
+    while (it.hasNext()) {
+      if (isNestTooClose(base, it.next())) {
+        it.remove();
+      }
+    }
+
+    if (base == null) {
+      return Optional.empty();
+    }
+    return Optional.of(base);
+  }
+
+  /**
    * Checks if the Position meets all conditions to hold a Nest.
    * @param world the world given
    * @param current the Position to check
@@ -62,6 +85,83 @@ public class NestCustomizer implements WorldCustomizer {
    * @return true if both conditions are met, false otherwise
    */
   private boolean isBorderEmptyAndResourceAvailable(World world, Position current) {
+    Set<Position> twoLayerNeighbours = getTwoLayerNeighbours(world, current);
+
+    if (isBorderEmpty(world, twoLayerNeighbours)) {
+
+      Set<Position> thirdLayer = getThreeLayerNeighbours(world, twoLayerNeighbours);
+
+      thirdLayer.removeAll(twoLayerNeighbours);
+
+      return isResourceAvailable(world, thirdLayer);
+    }
+
+    return false;
+  }
+
+  /**
+   * Checks if at least one Resource is available to the Nest.
+   * @param world the world to check in
+   * @param thirdLayer the Positions to check
+   * @return true if at least one Resource was found, false otherwise
+   */
+  private boolean isResourceAvailable(World world, Set<Position> thirdLayer) {
+    for (Position pos : thirdLayer) {
+      if (world.getTileByPosition(pos).getEntity() != null
+          && world.getTileByPosition(pos).getEntity().getTypeComponent().getCategory()
+          == Category.RESOURCE) {
+
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Gets all Positions surrounding the original Positions two-layered neighbours,
+   * making the third layer around it.
+   * @param world the world to search in
+   * @param twoLayerNeighbours the original Positions neighbours and second-neighbours
+   * @return a set containing the found neighbours
+   */
+  private Set<Position> getThreeLayerNeighbours(World world, Set<Position> twoLayerNeighbours) {
+    Set<Position> thirdLayer = new HashSet<>();
+
+    for (Position pos : twoLayerNeighbours) {
+
+      for (Position thirdNeighbour : pos.getNeighbours()) {
+        if (world.isPositionValidTile(thirdNeighbour)) {
+          thirdLayer.add(thirdNeighbour);
+        }
+      }
+    }
+    return thirdLayer;
+  }
+
+  /**
+   * Checks if the neighbours and the second-neighbours of a Position are all of type CAVE.
+   * @param world the world to search in
+   * @param twoLayerNeighbours the original Positions neighbours and second-neighbours
+   * @return true is all neighbours were CAVE and were empty, false otherwise
+   */
+  private boolean isBorderEmpty(World world, Set<Position> twoLayerNeighbours) {
+    boolean valid = true;
+
+    for (Position pos : twoLayerNeighbours) {
+      if (world.getTileByPosition(pos).isBlockedOrOccupied()) {
+        valid = false;
+      }
+    }
+    return valid;
+  }
+
+  /**
+   * Gets all neighbours and second-neighbours of a Position given.
+   * @param world the world to search in
+   * @param current the Position to check
+   * @return a set containing all neighbours and second-neighbours of the Position
+   */
+  private Set<Position> getTwoLayerNeighbours(World world, Position current) {
     Set<Position> twoLayerNeighbours = new HashSet<>();
 
     for (Position neighbour : current.getNeighbours()) {
@@ -77,62 +177,7 @@ public class NestCustomizer implements WorldCustomizer {
         }
       }
     }
-
-    boolean valid = true;
-
-    for (Position pos : twoLayerNeighbours) {
-      if (world.getTileByPosition(pos).isBlockedOrOccupied()) {
-        valid = false;
-      }
-    }
-
-    if (valid) {
-
-      Set<Position> thirdLayer = new HashSet<>();
-
-      for (Position pos : twoLayerNeighbours) {
-
-        for (Position thirdNeighbour : pos.getNeighbours()) {
-          if (world.isPositionValidTile(thirdNeighbour)) {
-            thirdLayer.add(thirdNeighbour);
-          }
-        }
-      }
-
-      thirdLayer.removeAll(twoLayerNeighbours);
-
-      for (Position pos : thirdLayer) {
-        if (world.getTileByPosition(pos).getEntity() != null
-            && world.getTileByPosition(pos).getEntity().getTypeComponent().getCategory()
-            == Category.RESOURCE) {
-
-          return true;
-        }
-      }
-
-      return false;
-    }
-
-    return false;
-  }
-
-  /**
-   * Returns a Position to place a Nest, chosen at random.
-   * @return the chosen Position
-   */
-  public Position getRandomNestPosition() {
-    Position base = basePositions.get(new Random().nextInt(basePositions.size() - 1));
-    basePositions.remove(base);
-
-    Iterator<Position> it = basePositions.iterator();
-
-    while (it.hasNext()) {
-      if (isNestTooClose(base, it.next())) {
-        it.remove();
-      }
-    }
-
-    return base;
+    return twoLayerNeighbours;
   }
 
   /**
