d8c42dd8900b03c2bc0e2e2ac3443232618de6dc
Attila Bagossy <bagossyattila@outlook.com>
2016-12-07 16:37:19 +0100

    Added a static factory method to OrderedList

--- server-core/engine/src/main/java/io/infectnet/server/engine/core/util/OrderedList.java
+++ server-core/engine/src/main/java/io/infectnet/server/engine/core/util/OrderedList.java
@@ -1,6 +1,7 @@
 package io.infectnet.server.engine.core.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
@@ -14,6 +15,24 @@ public class OrderedList<E extends Ordered> {
   private static final int FIRST = 0;
 
   private final List<E> backingList;
+
+  /**
+   * Produces a new {@code OrderedList} that contains the elements of the specified
+   * collection.
+   * @param elements the elements to be stored in the new {@code OrderedList}
+   * @param <E> the type of elements to be stored in the list
+   * @return a new {@&ode OrderedList}
+   * @throws NullPointerException if the passed collection is {@code null}
+   */
+  public static <E extends Ordered> OrderedList<E> of(Collection<E> elements) {
+    Collection<E> nonNullElements = Objects.requireNonNull(elements);
+
+    OrderedList<E> list = new OrderedList<>();
+
+    nonNullElements.forEach(list::add);
+
+    return list;
+  }
 
   /**
    * Constructs a new empty list.
